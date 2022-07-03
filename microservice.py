e0251575918acf02bbaa3bdc9b14efacc8101caa
Ross Heenan <30299958+Ross-Heenan@users.noreply.github.com>
2022-07-03 17:54:21 +0100

    First CircleCI Job

---
+++ microservice.py
@@ -0,0 +1,17 @@
+"""
+This is a basic hello world flask application.
+The application will get deployed to AWS via
+instructions/jobs in a CircleCI CI/CD pipeline.
+It will act as a microservice.
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    """ Default hello world endpoint"""
+    return 'Hello World'
+
+if __name__ == '__main__':
+    app.run(debug=True)
