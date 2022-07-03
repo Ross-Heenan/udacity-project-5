40ac0e240992f747449acbf2439cc2cd2c2d4fb2
Ross Heenan <30299958+Ross-Heenan@users.noreply.github.com>
2022-07-03 18:23:33 +0100

    change to Hello World text in application

--- microservice.py
+++ microservice.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """ Default hello world endpoint"""
-    return 'Hello World'
+    return 'Hello World - my name is Ross'
 
 if __name__ == '__main__':
     app.run(debug=True)
