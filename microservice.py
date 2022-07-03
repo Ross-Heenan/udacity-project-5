e6c4555cb2e1c2fa8d5b6d731990c442e48a9848
Ross Heenan <30299958+Ross-Heenan@users.noreply.github.com>
2022-07-03 18:50:29 +0100

    committing incomplete create-infra job for cloud backup

--- microservice.py
+++ microservice.py
@@ -2,7 +2,7 @@
 This is a basic hello world flask application.
 The application will get deployed to AWS via
 instructions/jobs in a CircleCI CI/CD pipeline.
-It will act as a microservice and will be
+The app will act as a microservice and will be
 containerised using docker and kubernetes.
 """
 from flask import Flask
