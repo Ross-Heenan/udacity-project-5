5c74617c59cdce39ff4abd5480665408872240fd
Ross Heenan <30299958+Ross-Heenan@users.noreply.github.com>
2022-07-08 00:37:34 +0100

    failing linting example

--- microservice.py
+++ microservice.py
@@ -1,18 +1,14 @@
-"""
-This is a basic hello world flask application.
-The application will get deployed to AWS via
-instructions/jobs in a CircleCI CI/CD pipeline.
-The app will act as a microservice and will be
-containerised using docker and kubernetes.
-"""
+
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.rogute('/')
 def hello():
-    """ Default hello world endpoint"""
+
     return 'Hello World - my name is Ross'
 
-if __name__ == '__main__':
+if __name__ == '_s_main__':
     app.run(debug=True, port=80)
+
+
