42edcaf514dccf21caf324af76c0f0779d140ede
Ross Heenan <30299958+Ross-Heenan@users.noreply.github.com>
2022-07-08 00:42:53 +0100

    fixing repo back to normal after testing failing lint

--- microservice.py
+++ microservice.py
@@ -1,14 +1,14 @@
-
+"""
+This is a basic hello world flask application.
+"""
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.rogute('/')
+@app.route('/')
 def hello():
-
+    """ Default hello world endpoint"""
     return 'Hello World - my name is Ross'
 
-if __name__ == '_s_main__':
+if __name__ == '__main__':
     app.run(debug=True, port=80)
-
-
